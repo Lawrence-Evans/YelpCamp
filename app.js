01beb21c2686eb7ec207ef20e64b9b03e7151bca
Lawrence-Evans <lawrenceevans309@gmail.com>
2024-01-05 13:16:08 +0000

    Section 51 half complete

--- app.js
+++ app.js
@@ -6,7 +6,16 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user')
 
+// Route requires
+const userRoutes = require('./routes/users');
+const campgroundsRoutes = require('./routes/campgrounds');
+const reviewsRoutes = require('./routes/reviews');
+
+// Database connection
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', {
     // Not sure if needed as no deprecation warnings
     // useNewUrlParser: true,
@@ -15,16 +24,12 @@ mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', {
     // useFindAndModify:false
 });
 
-// Route requires
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
-
-
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
     console.log("Database connected");
 });
+// Database connection
 
 const app = express();
 
@@ -36,6 +41,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')))
 
+//Session + Flash + Passport
 const sessionConfig = {
     secret: 'devsecret',
     resave: false,
@@ -50,14 +56,31 @@ const sessionConfig = {
 app.use(session(sessionConfig))
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
 })
+//Session + Flash + Passport
 
-app.use('/campgrounds', campgrounds)
-app.use('/campgrounds/:id/reviews', reviews)
+// Register User
+app.get('/fakeUser', async (req, res) => {
+    const user = new User({email: 'fake@gmail.com', username: 'fakerson'})
+    const newUser = await User.register(user, 'password')
+    res.send(newUser);
+})
+// Register User
+
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundsRoutes);
+app.use('/campgrounds/:id/reviews', reviewsRoutes);
 
 
 app.get('/', (req, res) => {
